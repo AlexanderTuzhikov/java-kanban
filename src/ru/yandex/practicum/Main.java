b49a170db08645fa5243918a138333039004bc69
Александр Тужиков <alehandro.tuzhikov@gmail.com>
2025-06-12 12:04:48 +0300

    Добавлены пакеты

---
+++ src/ru/yandex/practicum/Main.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum;
+
+import ru.yandex.practicum.manager.TaskManager;
+import ru.yandex.practicum.tasks.TaskStatus;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
